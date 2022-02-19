9d28d48019608a8bcd11b59c64c56143948ad8b5
ishikoro <nskwstkvita@gmail.com>
2022-02-20 01:21:31 +0900

    fix

--- docs/js/ssrplus_checker.js
+++ docs/js/ssrplus_checker.js
@@ -3,6 +3,7 @@ const GRP_HINATA_CLASS = "grp_hinata";
 const GRP_NAME_SAKURA = "櫻坂46";
 const GRP_NAME_HINATA = "日向坂46";
 var sakuraMusicNameArray = ["サイレントマジョリティー", "ガラスを割れ！"];
+var hinataMusicNameArray = ["キュン"];
 
 /**
  * ページ読み込み時
@@ -20,6 +21,7 @@ function callLoadCsv() {
     // 櫻坂csv読み込み
     LoadCsv(GRP_NAME_SAKURA, sakuraMusicNameArray);
     // 日向坂csv読み込み
+    LoadCsv(GRP_NAME_HINATA, hinataMusicNameArray);
 }
 
 async function LoadCsv(grpName, array) {
@@ -43,20 +45,24 @@ function makeArrayList(musicName, grpName, data) {
     // 文字列→行単位に変換
     var list = data.split("\n");
     var grpClass = GRP_SAKURA_CLASS;
-    if (grpName == GRP_NAME_HINATA) grpClass = GRP_HINATA_CLASS;
+    var tableId = "#sakura_list";
+    if (grpName == GRP_NAME_HINATA) {
+        grpClass = GRP_HINATA_CLASS;
+        tableId = "#hinata_list";
+    }
 
     var listStr = '';
     listStr += '<p name="scene_list">';
-    listStr += '  <table border="1">';
+    listStr += '  <table name="scene_table" border="1">';
     listStr += '    <thead>';
     listStr += '      <tr class="'+ grpClass + '">';
     listStr += '        <th colspan="5">' + musicName + '</th>';
     listStr += '      </tr>';
     listStr += '      <tr class="'+ grpClass + '">';
     listStr += '        <th class="scene_color">色</th>';
-    listStr += '        <th class="member_name">メンバー</th>';
-    listStr += '        <th class="ssr">SSR</th>';
-    listStr += '        <th class="ssr_plus">SSR+</th>';
+    listStr += '        <th>メンバー</th>';
+    listStr += '        <th>SSR</th>';
+    listStr += '        <th>SSR+</th>';
     listStr += '        <th class="has_checkbox">✅</th>';
     listStr += '      </tr>';
     listStr += '    </thead>';
@@ -68,11 +74,11 @@ function makeArrayList(musicName, grpName, data) {
         var rowStr = '';
         rowStr += '<tr>';
         rowStr += '  <td style="background-color: ' + row[0].replace('\r', '') + ';"></td>';
-        rowStr += '  <td>' + row[1].replace('\r', '') + '</td>';
-        rowStr += '  <td>' + row[2].replace('\r', '') + '</td>';
-        rowStr += '  <td>' + row[3].replace('\r', '') + '</td>';
+        rowStr += '  <td class="member_name">' + row[1].replace('\r', '') + '</td>';
+        rowStr += '  <td class="ssr">' + row[2].replace('\r', '') + '</td>';
+        rowStr += '  <td class="ssr_plus">' + row[3].replace('\r', '') + '</td>';
         rowStr += '  <td>';
-        rowStr += '    <input type="checkbox" style="transform: scale(1.3);">';
+        rowStr += '    <input name="has_scene_check" type="checkbox" style="transform: scale(1.3);" onclick="btnTestClick()">';
         rowStr += '  </td>';
         rowStr += '</tr>';
         listStr += rowStr;
@@ -81,18 +87,17 @@ function makeArrayList(musicName, grpName, data) {
 
     listStr += '  </table>';
     listStr += '</p>';
-    $('#sakura_list').append(listStr);
+    $(tableId).append(listStr);
 
     return true;
 }
 
 function btnTestClick() {
     //ボタンを押下した際にダウンロードする画像を作る
-    html2canvas(document.getElementById("target"),{
-        onrendered: function(canvas){
-            //aタグのhrefにキャプチャ画像のURLを設定
-            var imgData = canvas.toDataURL();
-            document.getElementById("ss").href = imgData;
-        }
+    html2canvas(document.querySelector("#target")).then(canvas => {
+        var imgData = canvas.toDataURL();
+        document.getElementById("ss").href = imgData;
     });
+
+    return false;
 }
